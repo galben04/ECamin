276b36c3de1229fbd90378d4bab318a83b090fc5
galben04 <anghelmihaialexandru@gmail.com>
2017-03-14 18:29:43 +0200

    Layout Prima Pagina

--- app/src/main/java/com/mihaia/ecamin/PaginaPrincipala.java
+++ app/src/main/java/com/mihaia/ecamin/PaginaPrincipala.java
@@ -2,12 +2,32 @@ package com.mihaia.ecamin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class PaginaPrincipala extends AppCompatActivity {
 
+    LinearLayout layoutMeniuSus;
+    Button butArataMeniu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagina_principala);
+
+        layoutMeniuSus = (LinearLayout) findViewById(R.id.layoutBaraMeniuSus);
+        butArataMeniu = (Button) findViewById(R.id.buttonArataMeniuSus);
+
+        butArataMeniu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(layoutMeniuSus.getVisibility() == View.VISIBLE)
+                    layoutMeniuSus.setVisibility(View.GONE);
+
+                else if(layoutMeniuSus.getVisibility() == View.GONE)
+                    layoutMeniuSus.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
